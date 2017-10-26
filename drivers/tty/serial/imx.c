e00044b2ffa5d718f183ea7988834999863f0bbc
Lukas Wunner <lukas@wunner.de>
2017-10-26 11:13:17 +0200

    serial: core: Support common rs485 binding for RTS polarity
    When a driver invokes the uart_get_rs485_mode() helper, set the RTS
    polarity to active high by default unless the newly introduced
    "rs485-rts-active-low" property was specified.
    
    imx contains a line to set the default RTS polarity to active high,
    it is now superfluous and hence deleted.
    
    omap-serial historically defaults to active low and supports an
    "rs485-rts-active-high" property to inverse the polarity.
    Retain that behavior for compatibility.
    
    Cc: Mark Jackson <mpfj@newflow.co.uk>
    Cc: Michał Oleszczyk <oleszczyk.m@gmail.com>
    Cc: Rafael Gago Castano <rgc@hms.se>
    Cc: Sascha Hauer <s.hauer@pengutronix.de>
    Cc: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    Signed-off-by: Lukas Wunner <lukas@wunner.de>

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -2078,7 +2078,6 @@ static int serial_imx_probe(struct platform_device *pdev)
 	sport->port.fifosize = 32;
 	sport->port.ops = &imx_pops;
 	sport->port.rs485_config = imx_rs485_config;
-	sport->port.rs485.flags |= SER_RS485_RTS_ON_SEND;
 	sport->port.flags = UPF_BOOT_AUTOCONF;
 	setup_timer(&sport->timer, imx_timeout, (unsigned long)sport);
 
