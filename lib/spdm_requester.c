5f3f80e3d4844ff1f90d4548aa5084bc5d35ea3e
Lukas Wunner <lukas@wunner.de>
2022-12-21 20:57:31 +0100

    spdm: Add support for mutual authentication
    Signed-off-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Signed-off-by: Lukas Wunner <lukas@wunner.de>

--- lib/spdm_requester.c
+++ lib/spdm_requester.c
@@ -148,6 +148,8 @@
 #define SPDM_AEAD_CHACHA20_POLY1305	BIT(2)
 #define SPDM_AEAD_SM4_GCM		BIT(3)
 
+#define SPDM_REQ_ALG_STRUCT_REQ_BASE_ASYM_ALG 4
+
 #define SPDM_REQ_ALG_STRUCT_KEY_SCHEDULE 5
 #define SPDM_KEY_SCHEDULE_SPDM		BIT(0)
 
@@ -848,7 +850,7 @@ static int spdm_parse_algs(struct spdm_state *spdm_state)
 }
 
 /* Maximum number of ReqAlgStructs sent by this implementation */
-#define SPDM_MAX_REQ_ALG_STRUCT 3
+#define SPDM_MAX_REQ_ALG_STRUCT 4
 
 static int spdm_negotiate_algs(struct spdm_state *spdm_state,
 			       void *transcript, size_t transcript_sz)
@@ -876,6 +878,7 @@ static int spdm_negotiate_algs(struct spdm_state *spdm_state,
 	if (spdm_state->version >= 0x12)
 		req->other_params_support = SPDM_OPAQUE_DATA_FMT_GENERAL;
 
+	/* ReqAlgStruct order shall be by AlgType (SPDM 1.1.0 margin no 186) */
 	req_alg_struct = (struct spdm_req_alg_struct *)(req + 1);
 	if (spdm_state->responder_caps & SPDM_KEY_EX_CAP) {
 		req_alg_struct[i++] = (struct spdm_req_alg_struct) {
@@ -888,12 +891,19 @@ static int spdm_negotiate_algs(struct spdm_state *spdm_state,
 			.alg_count = 0x20,
 			.alg_supported = cpu_to_le16(SPDM_AEAD_ALGOS),
 		};
+	}
+	if (spdm_state->responder_caps & SPDM_MUT_AUTH_CAP)
+		req_alg_struct[i++] = (struct spdm_req_alg_struct) {
+			.alg_type = SPDM_REQ_ALG_STRUCT_REQ_BASE_ASYM_ALG,
+			.alg_count = 0x20,
+			.alg_supported = cpu_to_le16(SPDM_ASYM_ALGOS),
+		};
+	if (spdm_state->responder_caps & SPDM_KEY_EX_CAP)
 		req_alg_struct[i++] = (struct spdm_req_alg_struct) {
 			.alg_type = SPDM_REQ_ALG_STRUCT_KEY_SCHEDULE,
 			.alg_count = 0x20,
 			.alg_supported = cpu_to_le16(SPDM_KEY_SCHEDULE_SPDM),
 		};
-	}
 	WARN_ON(i > SPDM_MAX_REQ_ALG_STRUCT);
 	req_sz = sizeof(*req) + i * sizeof(*req_alg_struct);
 	rsp_sz = sizeof(*rsp) + i * sizeof(*req_alg_struct);
