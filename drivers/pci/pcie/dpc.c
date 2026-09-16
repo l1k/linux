2de89b762d7933d926bb163b901805f72a1b19e9
Lukas Wunner <lukas@wunner.de>
2026-09-16 21:01:22 +0200

    PCI/DPC: Reinstate support for AER-incapable ports
    Downstream Port Containment does not mandate presence of an Advanced Error
    Reporting capability, so a Downstream Port may support DPC, but not AER
    (PCIe r7.0 sec 6.2.11.2).
    
    Commit 708b20003624 ("PCI/AER: Remove HEST/FIRMWARE_FIRST parsing for AER
    ownership") seemingly inadvertently constrained DPC to AER-supporting
    Downstream Ports by inserting a call to pcie_aer_is_native() in
    dpc_probe(), which implicitly checks for presence of an AER capability.
    
    Drop the call.  It is superfluous because it duplicates the conditions
    applied by the PCIe port service driver when it decides whether to
    instantiate a DPC port service (see get_port_device_capability()).
    If the conditions are not met, no port service is instantiated and
    dpc_probe() isn't executed.
    
    The only difference between the conditions in dpc_probe() and the ones in
    get_port_device_capability() is the check for the AER capability, so by
    removing the checks from dpc_probe(), DPC is supported on AER-incapable
    Downstream Ports again.
    
    Fixes: 708b20003624 ("PCI/AER: Remove HEST/FIRMWARE_FIRST parsing for AER ownership")
    Signed-off-by: Lukas Wunner <lukas@wunner.de>
    Cc: stable@vger.kernel.org # v5.8+

--- drivers/pci/pcie/dpc.c
+++ drivers/pci/pcie/dpc.c
@@ -480,9 +480,6 @@ static int dpc_probe(struct pcie_device *dev)
 	int status;
 	u16 cap;
 
-	if (!pcie_aer_is_native(pdev) && !pcie_ports_dpc_native)
-		return -ENOTSUPP;
-
 	status = devm_request_threaded_irq(device, dev->irq, dpc_irq,
 					   dpc_handler, IRQF_SHARED,
 					   "pcie-dpc", pdev);
